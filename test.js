e1673685f0d17a112b1279e42a04c8a4df280456
ryoukata <ryoya030528@gmail.com>
2020-03-21 18:27:57 +0900

    fix(#5) fix test.js

--- test.js
+++ test.js
@@ -2,20 +2,24 @@
 const todo = require('./index.js');
 const assert = require('assert');
 
-// todoとlistのテスト
-todo.todo('ノートを買う');
-todo.todo('ペンを買う');
-assert.deepEqual(todo.list(), ['ノートを買う','ペンを買う']);
+// テストを実行する前にtasks.jsonを削除
+const fs = require('fs');
+fs.unlink('./tasks.json', (err) => {
+    // todoとlistのテスト
+    todo.todo('ノートを買う');
+    todo.todo('ペンを買う');
+    assert.deepEqual(todo.list(), ['ノートを買う','ペンを買う']);
 
-// done と donelist のテスト
-todo.done('ペンを買う');
-assert.deepEqual(todo.list(), ['ノートを買う']);
-assert.deepEqual(todo.donelist(), ['ペンを買う']);
+    // done と donelist のテスト
+    todo.done('ペンを買う');
+    assert.deepEqual(todo.list(), ['ノートを買う']);
+    assert.deepEqual(todo.donelist(), ['ペンを買う']);
 
-// del のテスト
-todo.del('ノートを買う');
-todo.del('ペンを買う');
-assert.deepEqual(todo.list(), []);
-assert.deepEqual(todo.donelist(), []);
+    // del のテスト
+    todo.del('ノートを買う');
+    todo.del('ペンを買う');
+    assert.deepEqual(todo.list(), []);
+    assert.deepEqual(todo.donelist(), []);
 
-console.log('テストが正常に完了しました');
+    console.log('テストが正常に完了しました');
+});
